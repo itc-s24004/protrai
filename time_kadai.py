ea1d54b6b205339a1380701018e21494df29635b
s24004 <s24004@std.it-college.ac.jp>
2024-06-24 07:06:08 +0900

    課題:時計アプリの提出

---
+++ time_kadai.py
@@ -0,0 +1,28 @@
+#s24004
+#1秒ごとに
+
+
+import datetime     #時間
+import tkinter as tk#ウィンドウ
+
+
+def update_time():#時計の更新
+    now = datetime.datetime.now()
+    current_time = now.strftime("%Y年%m月%d日 %H時%M分%S秒")
+    
+    lbl.config(text=current_time)
+    root.after(1000, update_time)#自分自身を呼び出してループ
+
+
+root = tk.Tk()#ウィンドウを作成
+
+root.title("時計アプリ")
+
+lbl = tk.Label()
+lbl.config(text="", font=("Helvetica", 20))
+lbl.pack()
+
+
+update_time()#関数呼び出し
+
+root.mainloop()

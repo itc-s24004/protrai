00b152da02b1eff1f10db947870e5aaa10bb2b4b
s24004 <s24004@std.it-college.ac.jp>
2024-07-01 06:57:29 +0900

    課題v2に更新の提出

--- dispImage.py
+++ dispImage.py
@@ -1,5 +1,5 @@
 #s24004
-#画像ビューアー
+#画像ビューアーv2
 
 import tkinter as tk
 import tkinter.filedialog as fd
@@ -15,14 +15,23 @@ def openFile():
     fpath = fd.askopenfilename()
     if fpath:
         dispPhoto(fpath)
+        print(fpath)
+        lbl.config(text=f"{fpath} \n が開かれています")
 
 root = tk.Tk()
-root.geometry("400x350")
+root.title("画像ビューアー")
+root.geometry("550x450")
+
+lblTitle = tk.Label(text="画像表示アプリ バージョン2.0", font=("Helvetica", 25))
+lblTitle.pack()
+
 
 btn = tk.Button(text="ファイルを開く", command = openFile)
 imageLabel = tk.Label()
+lbl = tk.Label(text="画像が開かれていません", font=("Helvetica", 15))
 
 btn.pack()
 imageLabel.pack()
+lbl.pack()
 
 tk.mainloop()
